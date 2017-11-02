f0bcb55b7b7ab0a3f5c85a1079cea0340d1c22bb
Thomas Charbonnel <thomas@charbonnel.eu>
2017-11-02 22:23:26 +0800

    Catch basic exceptions

--- rshell.rb
+++ rshell.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
 
 loop do
-  print "> "
-  command = gets
-  eval command
+  begin
+    print "> "
+    command = gets.chomp
+
+    exit(0) if %w(exit quit).include? command
+
+    eval command
+  rescue => e
+    STDERR.puts e.message
+  rescue SyntaxError => e
+    STDERR.puts e.message
+  end
 end
