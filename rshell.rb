0231fba571e2927ed2b1de96bd3ebfd6e0dcf689
Thomas Charbonnel <thomas@charbonnel.eu>
2017-11-05 19:05:49 +0800

    Use readline

--- rshell.rb
+++ rshell.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
 
+require "readline"
+
 class Shell
   EXCEPTIONS_TO_CATCH = [ScriptError, StandardError]
 
   def self.loop
     begin
-      print "> "
-      command = gets.chomp
+      line = Readline.readline("> ", true)
 
-      exit(0) if %w(exit quit).include? command
+      exit(0) if %w(exit quit).include? line || line.nil?
 
-      puts eval command
+      puts eval line
     rescue *EXCEPTIONS_TO_CATCH => e
       STDERR.puts e.message
     end
